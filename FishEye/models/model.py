71d791a11c63cb8b15295a97da28a0a9a0101672
Eigil <eigilalbaek@gmail.com>
2024-01-11 13:53:04 +0100

    Initial CNN model architechture

--- FishEye/models/model.py
+++ FishEye/models/model.py
@@ -1,26 +1,48 @@
+from typing import Any
+
+import pytorch_lightning as L
 import torch
+from torch import nn, optim
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
+
+class FishNN(L.LightningModule):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.classifier = nn.Sequential(
+            torch.nn.Conv2d(3, 16, kernel_size=3, stride=2),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+            torch.nn.Linear(16 * 294 * 222, 10),
+        )
+
+        self.criterium = torch.nn.CrossEntropyLoss()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-        
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
+        return self.classifier(x)
 
-        Returns:
-            Output tensor with shape [N,out_features]
+    def training_step(self, batch: Any, batch_idx: int) -> Any:
+        data, target = batch
+        preds = self(data)
+        loss = self.criterium(preds, target)
+        return loss
 
-        """
-        return self.l2(self.r(self.l1(x)))
+    def validation_step(self, batch: Any, batch_idx: int) -> Any:
+        data, target = batch
+        preds = self(data)
+        loss = self.criterium(preds, target)
+
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def test_step(self, batch: Any, batch_idx: int) -> Any:
+        data, target = batch
+        preds = self(data)
+        loss = self.criterium(preds, target)
+        return loss
+
+    def configure_optimizers(self) -> Any:
+        return optim.Adam(self.parameters(), lr=1e-3)
+
+
+if __name__ == "__main__":
+    pass
