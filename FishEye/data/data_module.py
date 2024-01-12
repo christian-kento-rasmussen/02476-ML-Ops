f21b5edc0c4300cf7442e10b83063cf2f4446ffe
Christian Kento Rasmussen <christian.kento@gmail.com>
2024-01-12 10:49:08 +0100

    made typing for data python files

--- FishEye/data/data_module.py
+++ FishEye/data/data_module.py
@@ -1,26 +1,38 @@
+import os
+
 import pytorch_lightning as pl
 import torch
-import os
-from torch.utils.data import TensorDataset, DataLoader, random_split
+from torch.utils.data import DataLoader, TensorDataset, random_split
 
 
 class FishDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str = "data/processed", batch_size: int = 32):
+        """Inits FishDataModule.
+
+        Args:
+            data_dir (str, optional): location of processed data. Defaults to "data/processed".
+            batch_size (int, optional): batch size. Defaults to 32.
+        """
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.prepare_data_per_node = True
 
-
     def setup(self, stage: str):
-        images = torch.load(os.path.join(self.data_dir, "images.pt"))        
+        """Loads data from data_dir and splits into train, val, test sets.
+
+        Args:
+            stage (str): stage of training (fit, test, predict)
+        """
+        images = torch.load(os.path.join(self.data_dir, "images.pt"))
         labels = torch.load(os.path.join(self.data_dir, "labels.pt"))
         dataset = TensorDataset(images, labels)
         self.train, self.test, self.val = random_split(
-            dataset, [int(len(dataset)*0.8), int(len(dataset)*0.1), int(len(dataset)*0.1)], generator = torch.Generator().manual_seed(69)
+            dataset,
+            [int(len(dataset) * 0.8), int(len(dataset) * 0.1), int(len(dataset) * 0.1)],
+            generator=torch.Generator().manual_seed(69),
         )
-        
-        
+
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch_size)
 
@@ -36,10 +48,3 @@ class FishDataModule(pl.LightningDataModule):
     def teardown(self, stage: str):
         # Used to clean-up when the run is finished
         pass
-
-
-if __name__ == "__main__":
-    
-    pass
-
-    
--- FishEye/data/make_dataset.py
+++ FishEye/data/make_dataset.py
@@ -8,43 +8,46 @@ from PIL import Image
 from tqdm import tqdm
 
 
-def process_data(raw_data_path:str ="data/raw/NA_Fish_Dataset", processed_data_path:str="data/processed") -> None:
-    """_summary_
+def process_data(raw_data_path: str = "data/raw/NA_Fish_Dataset", processed_data_path: str = "data/processed") -> None:
+    """Processes the raw data and saves it to the processed_data_path
 
     Args:
-        raw_data_path (str, optional): _description_. Defaults to "data/raw/NA_Fish_Dataset".
-        processed_data_path (str, optional): _description_. Defaults to "data/processed".
+        raw_data_path (str, optional): Location of raw data folder . Defaults to "data/raw/NA_Fish_Dataset".
+        processed_data_path (str, optional): Location of processed data folder. Defaults to "data/processed".
     """
-
-
     folder_paths = glob(os.path.join(raw_data_path, "*"))
 
+    # maps label index to label name
     label_map = {}
 
     images = []
     labels = []
 
+    # iterates through each folder in raw_data_path and adds to images and labels lists
     for label_i, folder in tqdm(enumerate(folder_paths), total=len(folder_paths), desc="Processing data"):
         label_map[label_i] = folder.split("/")[-1]
         for image_path in glob(os.path.join(folder, "*")):
             image = Image.open(image_path)
-            image = image.resize((590, 445))
+            image = image.resize((590, 445))  # converts all images to the same size (smallest image is 590x445)
             image = image.convert("RGB")  # Convert image to RGB format
-            image = torch.Tensor(np.array(image))  # Convert image to torch tensor
+            image = torch.tensor(image)  # Convert image to torch tensor
+
             images.append(image)
             labels.append(torch.tensor(label_i))
 
     stacked_images = torch.stack(images)
-    stacked_images = stacked_images.permute(0, 3, 1, 2)
-    torch.save(stacked_images, os.path.join(processed_data_path, "images.pt"))
+    stacked_images = stacked_images.permute(0, 3, 1, 2)  # fixes the shape of the images
+
     stacked_labels = torch.stack(labels)
+
+    # saves data to processed_data_path
+    torch.save(stacked_images, os.path.join(processed_data_path, "images.pt"))
     torch.save(stacked_labels, os.path.join(processed_data_path, "labels.pt"))
+
+    # saves the mapping of labels to their names
     with open(os.path.join(processed_data_path, "label_map.json"), "w") as fp:
         json.dump(label_map, fp)
 
 
-
 if __name__ == "__main__":
-    # Get the data and process it
-
     process_data()
