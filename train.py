7f6af839fc65f6f6270f2060696851a151414dbd
Eigil <eigilalbaek@gmail.com>
2024-01-15 10:31:26 +0100

    added profiling

--- train.py
+++ train.py
@@ -1,17 +1,22 @@
 from FishEye.train_model import train
 import hydra
 from omegaconf import OmegaConf
+import torch.autograd.profiler as profiler
 
 
 @hydra.main(config_path="config", config_name="config")
 def main(cfg):
-    
-
     # Print the config
     if cfg.print_cfg:
         print(OmegaConf.to_yaml(cfg))
 
-    train(cfg)
+    if cfg.profile:
+        with profiler.profile(record_shapes=True) as prof:
+            train(cfg)
+        print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+
+    else:
+        train(cfg)
 
 
 if __name__ == "__main__":
