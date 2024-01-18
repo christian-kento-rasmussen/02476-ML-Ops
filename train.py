80db6433acf48143ed4381e37ba8f791907c1235
Eigil <eigilalbaek@gmail.com>
2024-01-18 12:34:25 +0100

    Added torch profiling as a notebook

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 import hydra
-import wandb
 from omegaconf import DictConfig, OmegaConf
-import torch.autograd.profiler as profiler
 
 from FishEye.train_model import train
 
@@ -17,13 +15,7 @@ def main(cfg: DictConfig):
     if cfg.print_cfg:
         print(OmegaConf.to_yaml(cfg))
 
-    if cfg.profile:
-        with profiler.profile(record_shapes=True) as prof:
-            train(cfg)
-        print(prof.key_averages().table(sort_by="self_cpu_time_total"))
-
-    else:
-        train(cfg)
+    train(cfg)
 
 
 if __name__ == "__main__":
